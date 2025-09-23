9ed8eab738b19e2354e386b0be00f1017eea53f0
ANDUS25 <anandchavan113@gmail.com>
2025-09-23 07:59:25 +0530

    Setup for the multer

--- src/Book/bookRouter.js
+++ src/Book/bookRouter.js
@@ -1,9 +1,27 @@
 import Express from "express";
+import path from "node:path";
 
 import bookRouterController from "./bookController.js";
+import multer from "multer";
 
 const bookRouter = Express.Router();
 
-bookRouter.get("/", bookRouterController);
+// we are using Multer as a middleware because express don't accept the file as a json it's a special type of data which can only handle by multer
+
+const uploadFile = multer({
+  // destination is for where to store file,need to give path
+  dest: path.resolve(path.__dirname, "../../Public/data/books"),
+  limits: { fileSize: 10 * 1204 * 1024 },
+});
+
+// upload.fields is for multiple file as here we are sharing 2 file main file and coverImage.
+bookRouter.get(
+  "/",
+  uploadFile.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  bookRouterController
+);
 
 export default bookRouter;
